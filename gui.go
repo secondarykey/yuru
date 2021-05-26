03c25c81b3f9396c3eab609375c0876ebb689dd0
secondarykey <secondarykey@gmail.com>
2021-05-27 06:24:58 +0900

    fix: added config init

--- gui.go
+++ gui.go
@@ -2,11 +2,17 @@ package yuru
 
 import (
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/secondarykey/yuru/config"
 	"github.com/secondarykey/yuru/window"
 	"golang.org/x/xerrors"
 )
 
 func Show(name string) error {
+
+	err := config.Set(name)
+	if err != nil {
+		return xerrors.Errorf("config.Set() error: %w", err)
+	}
 
 	win, err := window.New()
 	if err != nil {
